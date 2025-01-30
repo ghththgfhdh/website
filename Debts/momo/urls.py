b15dd897cc9d0c191e6115362998de8e0ba0cf3b
ghththgfhdh <drewanorange@gmail.com>
2025-01-30 05:45:37 -0500

    Update urls.py
    I HAVE TO DO THIS ALL MANUALLY

--- Debts/momo/urls.py
+++ Debts/momo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    path('weblogs/', views.PostList.as_view(), name='blogs'), #wants blog.html
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'), # wants momo/post_list
+] 
